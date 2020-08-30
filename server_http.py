8626c8bef637a73df60d5396c5d88031c44c60d2
PablitotilbaP <61791800+PablitotilbaP@users.noreply.github.com>
2020-08-30 17:38:22 +0200

    Add files via upload

--- server_http.py
+++ server_http.py
@@ -21,32 +21,30 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         # client_1.send(b"I am client .........hello")
         # client_1.close()
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        body_dic = json.loads(body.decode())["dic"]
-        # print(body_dic.split(",")[0],"\n\r",body_dic.split(",")[1][1:])
-        list = body_dic.split(":")
-
-        public_ip = list[0]
-        public_port = int(list[1])
-        print(public_ip)
-        print(public_port)
-
-        # print(body["public_port"])
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((public_ip, public_port))
-        client.send(b"I am server .........hello")
-        client.close()
-
-
-        self.send_response(200)
-        self.end_headers()
-        response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(body)
-        self.wfile.write(response.getvalue())
+    # def do_POST(self):
+    #     content_length = int(self.headers['Content-Length'])
+    #     body = self.rfile.read(content_length)
+    #     body_dic = json.loads(body.decode())["dic"]
+    #     list = body_dic.split(":")
+    #
+    #     public_ip = list[0]
+    #     public_port = int(list[1])
+    #     print(public_ip)
+    #     print(public_port)
+    #
+    #     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #     client.connect((public_ip, public_port))
+    #     client.send(b"I am server .........hello")
+    #     client.close()
+    #
+    #
+    #     self.send_response(200)
+    #     self.end_headers()
+    #     response = BytesIO()
+    #     response.write(b'This is POST request. ')
+    #     response.write(b'Received: ')
+    #     response.write(body)
+    #     self.wfile.write(response.getvalue())
 
 
 # print('Server listening on port: ',port)
